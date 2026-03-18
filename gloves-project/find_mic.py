7cbf6d15acd0627ee9b8bd20f6202f5bd5470151
Mariana Sabino <marisabmor@gmail.com>
2026-03-17 22:34:35 -0300

    reorganize project structure'

--- gloves-project/find_mic.py
+++
@@ -1,6 +0,0 @@
-import sounddevice as sd
-
-print("Available devices:")
-print(sd.query_devices())
-print("\nDefault input:", sd.query_devices(kind='input'))
-print("\nDefault output:", sd.query_devices(kind='output'))
